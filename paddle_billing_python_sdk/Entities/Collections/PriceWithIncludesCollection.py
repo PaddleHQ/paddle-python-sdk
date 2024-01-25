a3dce2cb32b90b517953c3a54ac03c2462bca6ae
Corey Regan <regan.corey@gmail.com>
2024-01-25 08:46:31 -0800

    circular imports suck

--- paddle_billing_python_sdk/Entities/Collections/PriceWithIncludesCollection.py
+++ paddle_billing_python_sdk/Entities/Collections/PriceWithIncludesCollection.py
@@ -1,11 +1,10 @@
 from paddle_billing_python_sdk.Entities.Collections.Collection import Collection
 
-from paddle_billing_python_sdk.Entities.PriceWithIncludes import PriceWithIncludes
-
 
 class PriceWithIncludesCollection(Collection):
     @classmethod
     def from_list(cls, items_data, paginator=None):
+        from paddle_billing_python_sdk.Entities.PriceWithIncludes import PriceWithIncludes
         items = [PriceWithIncludes.from_dict(item) for item in items_data]
         return cls(items, paginator)
 
