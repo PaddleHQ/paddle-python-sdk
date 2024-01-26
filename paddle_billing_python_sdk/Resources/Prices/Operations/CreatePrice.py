54a9cf81ecccfad1765d7d68504be90181c8a48c
Corey Regan <regan.corey@gmail.com>
2024-01-26 12:24:41 -0800

    formatting

--- paddle_billing_python_sdk/Resources/Prices/Operations/CreatePrice.py
+++ paddle_billing_python_sdk/Resources/Prices/Operations/CreatePrice.py
@@ -16,14 +16,14 @@ class CreatePrice:
     description:          str
     product_id:           str
     unit_price:           Money
-    name:                 str | None | Undefined                     = Undefined()
-    type:                 CatalogType | None | Undefined             = Undefined()
+    name:                 str                     | None | Undefined = Undefined()
+    type:                 CatalogType             | None | Undefined = Undefined()
     unit_price_overrides: list[UnitPriceOverride] | None | Undefined = Undefined()
-    tax_mode:             TaxMode | None | Undefined                 = Undefined()
-    trial_period:         TimePeriod | None | Undefined              = Undefined()
-    billing_cycle:        TimePeriod | None | Undefined              = Undefined()
-    quantity:             PriceQuantity | None | Undefined           = Undefined()
-    custom_data:          CustomData | None | Undefined              = Undefined()
+    tax_mode:             TaxMode                 | None | Undefined = Undefined()
+    trial_period:         TimePeriod              | None | Undefined = Undefined()
+    billing_cycle:        TimePeriod              | None | Undefined = Undefined()
+    quantity:             PriceQuantity           | None | Undefined = Undefined()
+    custom_data:          CustomData              | None | Undefined = Undefined()
 
 
     def get_parameters(self) -> dict:
