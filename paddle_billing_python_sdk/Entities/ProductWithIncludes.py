0d3780ad3ef2f70e0cc0c123c63f320f3ea2d240
Corey Regan <regan.corey@gmail.com>
2024-01-28 11:57:16 -0800

    formatting

--- paddle_billing_python_sdk/Entities/ProductWithIncludes.py
+++ paddle_billing_python_sdk/Entities/ProductWithIncludes.py
@@ -15,29 +15,29 @@ from paddle_billing_python_sdk.Entities.Shared.TaxCategory import TaxCategory
 
 @dataclass
 class ProductWithIncludes(Entity):
-    id:           str
-    name:         str
     description:  Optional[str]
-    type:         CatalogType | None
-    tax_category: TaxCategory
+    id:           str
     image_url:    Optional[str]
-    custom_data:  CustomData | None
-    status:       Status
-    created_at:   datetime | None
+    name:         str
     prices:       PriceWithIncludesCollection
+    status:       Status
+    tax_category: TaxCategory
+    type:         CatalogType | None
+    custom_data:  CustomData  | None
+    created_at:   datetime    | None
 
 
     @classmethod
     def from_dict(cls, data: dict) -> ProductWithIncludes:
         return ProductWithIncludes(
-            id           = data['id'],
-            name         = data['name'],
             description  = data.get('description'),
-            tax_category = TaxCategory(data['tax_category']),
+            id           = data['id'],
             image_url    = data.get('image_url'),
-            status       = Status(data['status']),
+            name         = data['name'],
             prices       = PriceWithIncludesCollection.from_list(data['prices']),
-            type         = CatalogType(data['type'])                  if data.get('type')        else None,
-            custom_data  = CustomData(data['custom_data'])            if data.get('custom_data') else None,
+            status       = Status(data['status']),
+            tax_category = TaxCategory(data['tax_category']),
             created_at   = datetime.fromisoformat(data['created_at']) if data.get('created_at')  else None,
+            custom_data  = CustomData(data['custom_data'])            if data.get('custom_data') else None,
+            type         = CatalogType(data['type'])                  if data.get('type')        else None,
         )
