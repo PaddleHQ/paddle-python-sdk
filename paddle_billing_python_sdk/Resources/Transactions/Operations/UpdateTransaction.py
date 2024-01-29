fb89ff2d881d7db3923c8761f969bf5cdf1e8da3
Corey Regan <regan.corey@gmail.com>
2024-01-28 22:07:58 -0800

    UpdateTransaction.items = Undefined()

--- paddle_billing_python_sdk/Resources/Transactions/Operations/UpdateTransaction.py
+++ paddle_billing_python_sdk/Resources/Transactions/Operations/UpdateTransaction.py
@@ -17,18 +17,18 @@ from paddle_billing_python_sdk.Entities.Transactions.TransactionTimePeriod      
 
 @dataclass
 class UpdateTransaction:
-    items:           list[TransactionCreateItem   | TransactionCreateItemWithPrice]
-    status:          StatusTransaction            | Undefined = Undefined()
-    customer_id:     str                   | None | Undefined = Undefined()
-    address_id:      str                   | None | Undefined = Undefined()
-    business_id:     str                   | None | Undefined = Undefined()
-    custom_data:     CustomData            | None | Undefined = Undefined()
-    currency_code:   CurrencyCode                 | Undefined = Undefined()
-    collection_mode: CollectionMode               | Undefined = Undefined()
-    discount_id:     str                   | None | Undefined = Undefined()
-    billing_details: BillingDetails        | None | Undefined = Undefined()
-    billing_period:  TransactionTimePeriod | None | Undefined = Undefined()
-    checkout:        Checkout              | None | Undefined = Undefined()
+    items:           list[TransactionCreateItem   | TransactionCreateItemWithPrice] = Undefined()
+    status:          StatusTransaction            | Undefined                       = Undefined()
+    customer_id:     str                   | None | Undefined                       = Undefined()
+    address_id:      str                   | None | Undefined                       = Undefined()
+    business_id:     str                   | None | Undefined                       = Undefined()
+    custom_data:     CustomData            | None | Undefined                       = Undefined()
+    currency_code:   CurrencyCode                 | Undefined                       = Undefined()
+    collection_mode: CollectionMode               | Undefined                       = Undefined()
+    discount_id:     str                   | None | Undefined                       = Undefined()
+    billing_details: BillingDetails        | None | Undefined                       = Undefined()
+    billing_period:  TransactionTimePeriod | None | Undefined                       = Undefined()
+    checkout:        Checkout              | None | Undefined                       = Undefined()
 
 
     def get_parameters(self) -> dict:
