07f783ac58ca3cc6cba3acfee32bba083a550229
Corey Regan <regan.corey@gmail.com>
2024-02-16 11:56:20 -0800

    paddle-php-sdk PR #37 NotificationSetting and Product incorrectly referenced collections instead of arrays

--- paddle_billing/Entities/NotificationSetting.py
+++ paddle_billing/Entities/NotificationSetting.py
@@ -2,7 +2,7 @@ from __future__  import annotations
 from dataclasses import dataclass
 
 from paddle_billing.Entities.Entity               import Entity
-from paddle_billing.Entities.Collections          import EventTypeCollection
+from paddle_billing.Entities.EventType            import EventType
 from paddle_billing.Entities.NotificationSettings import NotificationSettingType
 
 
@@ -15,7 +15,7 @@ class NotificationSetting(Entity):
     active:                   bool
     api_version:              int
     include_sensitive_fields: bool
-    subscribed_events:        EventTypeCollection
+    subscribed_events:        list[EventType]
     endpoint_secret_key:      str
 
 
@@ -29,6 +29,6 @@ class NotificationSetting(Entity):
             active                   = data['active'],
             api_version              = data['api_version'],
             include_sensitive_fields = data['include_sensitive_fields'],
-            subscribed_events        = EventTypeCollection.from_list(data['subscribed_events']),
+            subscribed_events        = [EventType.from_dict(event) for event in data.get('subscribed_events', [])],
             endpoint_secret_key      = data['endpoint_secret_key'],
         )
--- paddle_billing/Entities/Product.py
+++ paddle_billing/Entities/Product.py
@@ -2,9 +2,8 @@ from __future__  import annotations
 from dataclasses import dataclass
 from datetime    import datetime
 
-from paddle_billing.Entities.Entity      import Entity
-from paddle_billing.Entities.Collections import PriceCollection
-from paddle_billing.Entities.Shared      import CatalogType, CustomData, ImportMeta, Status, TaxCategory
+from paddle_billing.Entities.Entity import Entity
+from paddle_billing.Entities.Shared import CatalogType, CustomData, ImportMeta, Status, TaxCategory
 
 
 @dataclass
@@ -13,17 +12,19 @@ class Product(Entity):
     name:         str
     status:       Status
     tax_category: TaxCategory
-    description:  str             | None
-    image_url:    str             | None
-    created_at:   datetime        | None = None
-    custom_data:  CustomData      | None = None
-    import_meta:  ImportMeta      | None = None
-    prices:       PriceCollection | None = None
-    type:         CatalogType     | None = None
+    description:  str         | None
+    image_url:    str         | None
+    created_at:   datetime    | None = None
+    custom_data:  CustomData  | None = None
+    import_meta:  ImportMeta  | None = None
+    prices:       list[Price] | None = None
+    type:         CatalogType | None = None
 
 
     @classmethod
     def from_dict(cls, data: dict) -> Product:
+        from paddle_billing.Entities.Price  import Price  # prevents circular import from Entities/Products.py
+
         return Product(
             description  = data.get('description'),
             id           = data['id'],
@@ -31,10 +32,9 @@ class Product(Entity):
             name         = data['name'],
             status       = Status(data['status']),
             tax_category = TaxCategory(data['tax_category']),
-            prices       = PriceCollection.from_list(data['prices'])  if data.get('prices')      else [],
+            prices       = [Price.from_dict(price) for price in data.get('prices', [])],
             type         = CatalogType(data['type'])                  if data.get('type')        else None,
             custom_data  = CustomData(data['custom_data'])            if data.get('custom_data') else None,
             created_at   = datetime.fromisoformat(data['created_at']) if data.get('created_at')  else None,
             import_meta  = ImportMeta.from_dict(data['import_meta'])  if data.get('import_meta') else None,
         )
-
