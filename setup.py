7de839c07a2d2db842c3a216ea34d7074020319e
Corey Regan <regan.corey@gmail.com>
2024-02-26 13:05:59 -0800

    Development Status :: 4 - Beta

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
 
     classifiers = [
         # Full list: https://pypi.org/classifiers/
-        'Development Status :: 3 - Beta',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.11',
