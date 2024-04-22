d5476451ece0589a6875498047a9f9c254b34718
Victor Fernandez Saborit <victorfs.sp@gmail.com>
2024-04-22 17:30:30 +0200

    Prepare repo

--- paddle_billing/Client.py
+++ paddle_billing/Client.py
@@ -187,7 +187,7 @@ class Client:
             'Authorization':  f"Bearer {self.__api_key}",
             'Content-Type':   'application/json',
             'Paddle-Version': str(self.use_api_version),
-            'User-Agent':     'paddle-python-sdk',
+            'User-Agent':     'PaddleSDK/python 0.1.0',
         })
 
         # Configure retries
