d22ea489649ecced4b8e91522292632bc925495b
David Grayston <david.grayston@paddle.com>
2024-09-03 21:16:47 +0100

    fix: Release 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    version         = '0.2.1',
+    version         = '0.2.2',
 
     author          = 'Paddle and contributors',
     author_email    = 'team-dx@paddle.com',
