e123a27607abdd6bd7ada98d71112df77aaca6d8
David Grayston <david.grayston@paddle.com>
2024-11-12 22:22:45 +0000

    feat: Remove card and paypal from payment method notification entities

--- paddle_billing/Notifications/Entities/PaymentMethod.py
+++ paddle_billing/Notifications/Entities/PaymentMethod.py
@@ -4,8 +4,6 @@ from datetime import datetime
 
 from paddle_billing.Notifications.Entities.Entity import Entity
 from paddle_billing.Notifications.Entities.Shared import (
-    Card,
-    Paypal,
     SavedPaymentMethodOrigin,
     SavedPaymentMethodType,
 )
@@ -17,8 +15,6 @@ class PaymentMethod(Entity):
     customer_id: str
     address_id: str
     type: SavedPaymentMethodType | None
-    card: Card | None
-    paypal: Paypal | None
     origin: SavedPaymentMethodOrigin
     saved_at: datetime
     updated_at: datetime
@@ -30,8 +26,6 @@ class PaymentMethod(Entity):
             customer_id=data["customer_id"],
             address_id=data["address_id"],
             type=SavedPaymentMethodType(data["type"]),
-            card=Card.from_dict(data["card"]) if data.get("card") else None,
-            paypal=Paypal.from_dict(data["paypal"]) if data.get("paypal") else None,
             origin=SavedPaymentMethodOrigin(data["origin"]),
             saved_at=datetime.fromisoformat(data["saved_at"]),
             updated_at=datetime.fromisoformat(data["updated_at"]),
--- paddle_billing/Notifications/Entities/PaymentMethodDeleted.py
+++ paddle_billing/Notifications/Entities/PaymentMethodDeleted.py
@@ -4,8 +4,6 @@ from datetime import datetime
 
 from paddle_billing.Notifications.Entities.Entity import Entity
 from paddle_billing.Notifications.Entities.Shared import (
-    Card,
-    Paypal,
     SavedPaymentMethodDeletionReason,
     SavedPaymentMethodOrigin,
     SavedPaymentMethodType,
@@ -18,8 +16,6 @@ class PaymentMethodDeleted(Entity):
     customer_id: str
     address_id: str
     type: SavedPaymentMethodType | None
-    card: Card | None
-    paypal: Paypal | None
     origin: SavedPaymentMethodOrigin
     saved_at: datetime
     updated_at: datetime
@@ -32,8 +28,6 @@ class PaymentMethodDeleted(Entity):
             customer_id=data["customer_id"],
             address_id=data["address_id"],
             type=SavedPaymentMethodType(data["type"]),
-            card=Card.from_dict(data["card"]) if data.get("card") else None,
-            paypal=Paypal.from_dict(data["paypal"]) if data.get("paypal") else None,
             origin=SavedPaymentMethodOrigin(data["origin"]),
             saved_at=datetime.fromisoformat(data["saved_at"]),
             updated_at=datetime.fromisoformat(data["updated_at"]),
