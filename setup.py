d51de983b025d65986a0fe6887fa482131a74610
David Grayston <david.grayston@paddle.com>
2024-11-14 15:17:35 +0000

    docs: Release 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    version="1.0.0",
+    version="1.1.0",
     author="Paddle and contributors",
     author_email="team-dx@paddle.com",
     description="Paddle's Python SDK for Paddle Billing",
