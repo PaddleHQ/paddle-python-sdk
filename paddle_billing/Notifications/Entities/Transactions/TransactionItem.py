1920ebe33bf3e521027bb6ead533d5da0e378f4a
David Grayston <david.grayston@paddle.com>
2024-11-19 21:16:46 +0000

    test: Add coverage for simulation entity payloads

--- paddle_billing/Notifications/Entities/Transactions/TransactionItem.py
+++ paddle_billing/Notifications/Entities/Transactions/TransactionItem.py
@@ -4,10 +4,16 @@ from dataclasses import dataclass
 from paddle_billing.Notifications.Entities.Price import Price
 
 from paddle_billing.Notifications.Entities.Shared.Proration import Proration
+from paddle_billing.JsonExclude import json_exclude
 
 
 @dataclass
+@json_exclude(["price_id"])
 class TransactionItem:
+    """
+    Deprecated: price_id is no longer returned on transaction items.
+    """
+
     price_id: str | None
     price: Price
     quantity: int
