aaf89fb0fa86404df145f446d1e9698be1b8dc28
davidgrayston-paddle <david.grayston@paddle.com>
2025-08-15 11:46:16 +0100

    feat: Added support for filtering events by event_type in event list operation (#117)

---
+++ tests/Unit/Resources/Events/Operations/test_ListEvents.py
@@ -0,0 +1,13 @@
+from pytest import raises
+
+from build.lib.paddle_billing.Resources.Events.Operations import ListEvents
+
+from paddle_billing.Exceptions.SdkExceptions.InvalidArgumentException import InvalidArgumentException
+
+
+class TestListEvents:
+    def test_raises_invalid_argument_exception_for_invalid_event_types(self):
+        with raises(InvalidArgumentException) as exception_info:
+            ListEvents(event_types=["invalid.event"])
+
+        assert str(exception_info.value) == "Expected 'event_types' to only contain type 'EventTypeName' ('str' given)"
