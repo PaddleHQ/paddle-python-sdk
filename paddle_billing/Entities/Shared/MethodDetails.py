b07e081b50c843cd6441357347d4cb0074bd066a
davidgrayston-paddle <david.grayston@paddle.com>
2025-11-12 09:24:19 +0000

    feat: Added additional support for local Korean payment methods (#129)

--- paddle_billing/Entities/Shared/MethodDetails.py
+++ paddle_billing/Entities/Shared/MethodDetails.py
@@ -5,13 +5,15 @@ from typing import Any
 from paddle_billing.Entities.Shared.Card import Card
 from paddle_billing.Entities.Shared.PaymentMethodType import PaymentMethodType
 from paddle_billing.Entities.Shared.PaymentMethodUnderlyingDetails import PaymentMethodUnderlyingDetails
+from paddle_billing.Entities.Shared.SouthKoreaLocalCard import SouthKoreaLocalCard
 
 
 @dataclass
 class MethodDetails:
     type: PaymentMethodType
     card: Card | None
-    underlying_details: PaymentMethodUnderlyingDetails | None
+    underlying_details: PaymentMethodUnderlyingDetails | None  # deprecated
+    south_korea_local_card: SouthKoreaLocalCard | None
 
     @staticmethod
     def from_dict(data: dict[str, Any]) -> MethodDetails:
@@ -21,6 +23,11 @@ class MethodDetails:
             (
                 PaymentMethodUnderlyingDetails.from_dict(data["underlying_details"])
                 if data.get("underlying_details")
+                else None
+            ),
+            (
+                SouthKoreaLocalCard.from_dict(data["south_korea_local_card"])
+                if data.get("south_korea_local_card")
                 else None
             ),
         )
---
+++ paddle_billing/Entities/Shared/SouthKoreaLocalCard.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Any
+
+from paddle_billing.Entities.Shared.SouthKoreaLocalCardType import SouthKoreaLocalCardType
+
+
+@dataclass
+class SouthKoreaLocalCard:
+    type: SouthKoreaLocalCardType
+    last4: str
+
+    @staticmethod
+    def from_dict(data: dict[str, Any]) -> SouthKoreaLocalCard:
+        return SouthKoreaLocalCard(
+            type=SouthKoreaLocalCardType(data["type"]),
+            last4=data["last4"],
+        )
