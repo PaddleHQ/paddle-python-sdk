aac79c05e8d619f9435c9b5c161de39a86db054f
David Grayston <david.grayston@paddle.com>
2026-07-20 22:59:54 +0100

    Sync ApiKeyPermission enum with public API scopes
    The enum was missing several public permission scopes for features the SDK
    already supports: checkout_domain.read/write, client_token.read/write, and
    metrics.read. Add them so api_key.* webhook payloads carrying these scopes
    hydrate as known permissions.

--- tests/Unit/Entities/Notifications/test_ApiKeyPermission.py
+++ tests/Unit/Entities/Notifications/test_ApiKeyPermission.py
@@ -1,5 +1,7 @@
 from json import loads
 
+from pytest import mark
+
 from paddle_billing.Notifications.Entities.ApiKey import ApiKey
 from paddle_billing.Notifications.Entities.ApiKeys import ApiKeyPermission
 
@@ -7,14 +9,31 @@ from tests.Utils.ReadsFixture import ReadsFixtures
 
 
 class TestApiKeyPermission:
-    def test_subscription_history_read_is_a_known_permission(self):
-        permission = ApiKeyPermission("subscription_history.read")
+    @mark.parametrize(
+        "value, expected_name",
+        [
+            ("checkout_domain.read", "CheckoutDomainRead"),
+            ("checkout_domain.write", "CheckoutDomainWrite"),
+            ("client_token.read", "ClientTokenRead"),
+            ("client_token.write", "ClientTokenWrite"),
+            ("metrics.read", "MetricsRead"),
+            ("subscription_history.read", "SubscriptionHistoryRead"),
+        ],
+    )
+    def test_permission_is_known(self, value, expected_name):
+        permission = ApiKeyPermission(value)
 
         assert permission.is_known()
-        assert permission == ApiKeyPermission.SubscriptionHistoryRead
-        assert permission.name == "SubscriptionHistoryRead"
+        assert permission.name == expected_name
+        assert permission == getattr(ApiKeyPermission, expected_name)
 
-    def test_api_key_entity_hydrates_subscription_history_read_permission(self):
+    def test_api_key_entity_hydrates_new_permissions(self):
         entity = ApiKey.from_dict(loads(ReadsFixtures.read_raw_json_fixture("notification/entity/api_key.created")))
 
-        assert ApiKeyPermission.SubscriptionHistoryRead in entity.permissions
+        for permission in [
+            ApiKeyPermission.CheckoutDomainRead,
+            ApiKeyPermission.ClientTokenRead,
+            ApiKeyPermission.MetricsRead,
+            ApiKeyPermission.SubscriptionHistoryRead,
+        ]:
+            assert permission in entity.permissions
